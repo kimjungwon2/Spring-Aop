b1a657a1561d0327aeb4b26e47fe0bfc706035a3
jungwon <mac20010@naver.com>
2023-08-14 16:42:02 +0900

    FEAT : AOP 예제 만들기

---
+++ src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,36 @@
+package hello.aop;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}",
+                AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}",
+                AopUtils.isAopProxy(orderRepository));
+    }
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
