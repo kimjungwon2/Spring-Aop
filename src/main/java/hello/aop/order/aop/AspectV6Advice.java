0b2e28d6527937fe6c59d2b0a7786f4ed451c2eb
jungwon <mac20010@naver.com>
2023-08-30 17:20:02 +0900

    FEAT : 여러 어드바이스 구현

--- src/main/java/hello/aop/order/aop/AspectV6Advice.java
+++ src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -3,6 +3,9 @@ package hello.aop.order.aop;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -32,4 +35,20 @@ public class AspectV6Advice {
     public void doBefore(JoinPoint joinPoint){
         log.info("[before] {}",joinPoint.getSignature());
     }
+
+    @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()",returning = "result")
+    public void doReturn(JoinPoint joinPoint,Object result){
+        log.info("[return] {} return={}",joinPoint.getSignature(),result);
+    }
+
+    @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()",throwing = "ex")
+    public void doThrowing(JoinPoint joinPoint, Exception ex){
+        log.info("[ex] {} message={}",ex);
+    }
+
+    @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint){
+        log.info("[after] {}",joinPoint.getSignature());
+    }
+
 }
