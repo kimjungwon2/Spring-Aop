458cfe402efc10bb015d47ea1dab05d15d56d9d3
jungwon <mac20010@naver.com>
2023-09-16 19:00:30 +0900

    TEST : AOP 개선된 매개변수 전달

--- src/test/java/hello/aop/pointcut/ParameterTest.java
+++ src/test/java/hello/aop/pointcut/ParameterTest.java
@@ -1,9 +1,14 @@
 package hello.aop.pointcut;
 
 import hello.aop.member.MemberService;
+import hello.aop.member.annotation.ClassAop;
+import hello.aop.member.annotation.MethodAop;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +29,8 @@ public class ParameterTest {
         memberService.hello("helloA");
     }
 
+    @Slf4j
+    @Aspect
     static class ParameterAspect {
 
         @Pointcut("execution(* hello.aop.member..*.*(..))")
@@ -36,6 +43,38 @@ public class ParameterTest {
             log.info("[logArgs1]{}, arg={}", joinPoint.getSignature(), arg1);
 
             return joinPoint.proceed();
+        }
+
+        @Around("allMember() && args(arg,..)")
+        public Object logArgs2(ProceedingJoinPoint joinPoint, Object arg) throws Throwable {
+            log.info("[logArgs2]{}, arg={}", joinPoint.getSignature(), arg);
+
+            return joinPoint.proceed();
+        }
+
+        @Before("allMember() && args(arg,..)")
+        public void logArgs3(String arg){
+            log.info("[logArgs3]{}, arg={}", arg);
+        }
+
+        @Before("allMember() && this(obj)")
+        public void thisArgs(JoinPoint joinPoint, MemberService obj){
+            log.info("[this]{}, obj={}",joinPoint.getSignature(),obj.getClass());
+        }
+
+        @Before("allMember() && target(obj)")
+        public void targetArgs(JoinPoint joinPoint, MemberService obj){
+            log.info("[target]{}, obj={}",joinPoint.getSignature(),obj.getClass());
+        }
+
+        @Before("allMember() && @within(annotation)")
+        public void atWithin(JoinPoint joinPoint, ClassAop annotation){
+            log.info("[@within]{}, obj={}",joinPoint.getSignature(),annotation);
+        }
+
+        @Before("allMember() && @annotation(annotation)")
+        public void atAnnotation(JoinPoint joinPoint, MethodAop annotation){
+            log.info("[@annotation]{}, annotationValue={}",joinPoint.getSignature(), annotation.value());
         }
     }
 }
