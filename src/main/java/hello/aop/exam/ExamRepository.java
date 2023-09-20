c3bd86375fb5cf18c46b23d84135d8439fa0b7eb
jungwon <mac20010@naver.com>
2023-09-20 17:03:35 +0900

    FEAT : 재시도 AOP

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,7 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
+import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -7,6 +9,8 @@ public class ExamRepository {
 
     private static int seq = 0;
 
+    @Trace
+    @Retry(4)
     public String save(String itemId){
         seq++;
 
