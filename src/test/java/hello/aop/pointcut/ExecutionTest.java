f479e282e5b886d0c63c137831bded5e1f771f98
jungwon <mac2001035@gmail.com>
2024-06-13 04:09:22 +0900

    FEAT: Execution Pointcut.

--- src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -3,10 +3,13 @@ package hello.aop.pointcut;
 import hello.aop.member.MemberServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
 import java.lang.reflect.Method;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Slf4j
 public class ExecutionTest {
@@ -24,5 +27,15 @@ public class ExecutionTest {
         //execution(* ..package..Class.)
         log.info("helloMethod={}", helloMethod);
 
+    }
+
+    @DisplayName("")
+    @Test
+    void test(){
+      //given
+        pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+
+      //then
     }
 }
