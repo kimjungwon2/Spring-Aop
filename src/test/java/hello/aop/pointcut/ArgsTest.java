73805ed3990157e5d3c9b585cd02cc15dce20950
jungwon <mac2001035@gmail.com>
2024-06-17 21:30:06 +0900

    FEAT: args 포인트컷.

---
+++ src/test/java/hello/aop/pointcut/ArgsTest.java
@@ -0,0 +1,45 @@
+package hello.aop.pointcut;
+
+
+import hello.aop.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ArgsTest {
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException{
+        helloMethod = MemberServiceImpl.class.getMethod("hello",String.class);
+    }
+
+    private AspectJExpressionPointcut pointcut(String expression){
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+        return pointcut;
+    }
+
+    @DisplayName("hello(String)과 매칭")
+    @Test
+    void args(){
+        assertThat(pointcut("args(String)")
+                .matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(Object)")
+                .matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args()")
+                .matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(..)")
+                .matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(*)")
+                .matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(String,..)")
+                .matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+}
