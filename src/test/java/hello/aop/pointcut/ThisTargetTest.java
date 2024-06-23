0205f248c89bd2940df0b47dcbc2b6cd61a14e59
jungwon <mac2001035@gmail.com>
2024-06-24 08:47:19 +0900

    FEAT: JDK 동적 프록시 적용.

--- src/test/java/hello/aop/pointcut/ThisTargetTest.java
+++ src/test/java/hello/aop/pointcut/ThisTargetTest.java
@@ -13,7 +13,8 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @Import(ThisTargetTest.ThisTargetAspect.class)
-@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB
+@SpringBootTest(properties = "spring.aop.proxy-target-class=false") // JDK 동적 프록시
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB
 public class ThisTargetTest {
 
     @Autowired
